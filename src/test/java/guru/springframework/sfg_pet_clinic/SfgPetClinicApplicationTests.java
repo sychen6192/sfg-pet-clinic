adef00d8bae9db7aa519e3d2e4f72d0539011ad6
Jack SY Chen <jack_sy_chen@trendmicro.com>
2024-08-11 15:09:11 +0800

    [feat] initialize project

---
+++ src/test/java/guru/springframework/sfg_pet_clinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

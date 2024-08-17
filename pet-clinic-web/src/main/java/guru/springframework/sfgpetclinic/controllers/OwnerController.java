50439a4c33b1e133c8918f56c14f9d7e68b4e421
Jack SY Chen <jack_sy_chen@trendmicro.com>
2024-08-17 16:59:34 +0800

    fix empty owners router

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,12 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listVets() {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
 
         return "owners/index";
     }

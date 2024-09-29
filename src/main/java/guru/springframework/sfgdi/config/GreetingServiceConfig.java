6efaba0d384fb9057a7e48e6b0924abd85baaf0f
Jack SY Chen <jack_sy_chen@trendmicro.com>
2024-09-29 17:41:54 +0800

    use application.properties to env

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import guru.springframework.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
 

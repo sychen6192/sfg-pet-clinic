5939de0db52fbb3cf4b372a4620904d52d604f36
Jack SY Chen <jack_sy_chen@trendmicro.com>
2024-09-29 18:08:20 +0800

    properties binding

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -14,14 +14,12 @@ public class GreetingServiceConfig {
 
     @Bean
     FakeDataSource fakeDataSource(
-            @Value("${guru.username}") String username,
-            @Value("${guru.password}") String password,
-            @Value("${guru.jdbcurl}") String jdbcurl
+           SfgConfiguration sfgConfiguration
     ) {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
-        fakeDataSource.setPassword(password);
-        fakeDataSource.setJdbcurl(jdbcurl);
+        fakeDataSource.setUsername(sfgConfiguration.getUsername());
+        fakeDataSource.setPassword(sfgConfiguration.getPassword());
+        fakeDataSource.setJdbcurl(sfgConfiguration.getJdbcurl());
         return fakeDataSource;
     }
 
